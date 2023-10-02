122d6bc9fad7641040122fb629fb115eae965851
Vrglab <vrglab1@gmail.com>
2023-10-02 13:01:35 +0200

    init

---
+++ Generate.bat
@@ -0,0 +1,1 @@
+Programs\premake\premake5.exe vs2022
---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "LowpEngine"
+	architecture "x64"
+
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%packages/includes"
+	}
+
+	filter "system:windows"
+		cppdialect "C++22"
+		staticruntime "On"
+		systemversion "latest"
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Editor")
+		}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
+		
+		
+project "LowpEngine"
+	location "LowpEngine"
+	kind "SharedLib"
+	language "C#"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.cs"
+	}
+	
+	links
+	{
+		"Engine"
+	}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
+		
+project "Editor"
+	location "Editor"
+	kind "ConsoleApp"
+	language "C#"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.cs"
+	}
+
+	links
+	{
+		"LowpEngine"
+	}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		symbols "On"
