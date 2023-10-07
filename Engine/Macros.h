cdaa6a2c4eb362699d46b84a6791d2306c6dea24
Vrglab <vrglab1@gmail.com>
2023-10-08 00:32:24 +0200

    made macros better

--- Engine/Macros.h
+++ Engine/Macros.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #ifdef  _lpEngine_
+
+//LP_EXPORT
 #ifdef _WIN32
   #define LP_Export __declspec(dllexport)
 #else
-  #define LP_Export
+  #define LP_Export __attribute__((visibility("default")))
 #endif
+
+//LP_EXTERN
 #define LP_Extern extern "C"
 #endif //  _lpEngine_
