ae2dd6108dcee71b81b21dd42d2e4da109ec2e43
Vrglab <vrglab1@gmail.com>
2023-10-07 17:35:29 +0200

    Removed c++ console texts

--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -27,35 +27,24 @@ void Window::ProcessEvents()
 
 void Window::Open()
 {
-	cout << "Opening window" << endl;
 	if (SDL_Init(0) != 0)
 	{
-		cout << "sdl init failed" << endl;
 		return;
 	}
 
 	if(info == nullptr)
 	{
-		cout << "info is null" << endl;
 		return;
 	}
 
-	cout << "sdl start creating window" << endl;
-	cout << info->name.c_str() << endl;
-	cout << info->resolution.width << endl;
-	cout << info->resolution.height << endl;
 	sdl_window = SDL_CreateWindow(info->name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, info->resolution.width, info->resolution.height, info->flags);
 
-	cout << "sdl_window made checking if we successeded" << endl;
 	if (!sdl_window) {
 		// Handle window creation error
-		cout << "sdl_window failed its creation phase" << endl;
 		SDL_Quit();
 		return;
 	}
 
-
-	cout << "bgfx starting" << endl;
 	Init init_;
 
 	init_.type = getRendererType();
