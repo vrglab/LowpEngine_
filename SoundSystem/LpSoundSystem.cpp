20f579758f1a43ea0ab4d647e237144a5165c044
Vrglab <Vrglab1@gmail.com>
2023-10-18 21:11:17 +0200

    mistake's were made

--- SoundSystem/LpSoundSystem.cpp
+++ SoundSystem/LpSoundSystem.cpp
@@ -4,7 +4,7 @@
 LP_Export int InitSoundSystem(SoundSystemInitInfo initInfo, SoundSystem* system)
 {
 	system = new SoundSystem();
-	if (initInfo.be_Type == SoundSystemBackendType::Fmod) {
+	if (initInfo.be_Type == SoundSystemBackendType::Fmod) { 
 
 		FMOD::System* fmod_system;
 		FMOD_RESULT fmod_result;
