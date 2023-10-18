2b1b6c05c446e84bb14973313ab236acdb034549
Vrglab <vrglab1@gmail.com>
2023-10-18 09:17:09 +0200

    Copyright comments added

--- Engine/LowpEngine.h
+++ Engine/LowpEngine.h
@@ -1,5 +1,11 @@
+/* ======================================================================================== */
+/* LowpEngine - Core C/C++ header file.														*/
+/*                                                                                          */
+/*  Copyright (c) 2023-Present Arad Bozorgmehr (Vrglab)                                     */
+/*                                                                                          */
+/* Authors: Arad Bozorgmehr(Vrglab)                                                         */
+/* ======================================================================================== */
 #pragma once
-
 
 #ifndef _lpEngine_ 
 #define _lpEngine_
