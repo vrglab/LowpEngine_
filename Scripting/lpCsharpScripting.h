96f37c5130d8456c8d528817f1fc4f61dd6b61f0
Vrglab <vrglab1@gmail.com>
2023-10-20 14:16:18 +0200

    fixed CSharpMonoBehaviour not being defined

--- Scripting/lpCsharpScripting.h
+++ Scripting/lpCsharpScripting.h
@@ -15,7 +15,28 @@
 #include <mono/metadata/assembly.h>
 #include <mono/metadata/debug-helpers.h>
 #include <LowpCommons.h>
-#include "CSharpMonoBehaviour.h"
+
+class CSharpMonoBehaviour
+{
+public:
+	MonoObject* LoadedClass;
+	MonoMethod* Update;
+	MonoMethod* Start;
+	MonoMethod* Awake;
+	MonoMethod* OnDestroy;
+	MonoMethod* OnEnable;
+	MonoMethod* OnDisable;
+	~CSharpMonoBehaviour()
+	{
+		delete LoadedClass;
+		delete Update;
+		delete Start;
+		delete Awake;
+		delete OnDestroy;
+		delete OnEnable;
+		delete OnDisable;
+	}
+};
 
 struct LoadedMonoBehaviours {
 	std::list<CSharpMonoBehaviour*> behaviours;
